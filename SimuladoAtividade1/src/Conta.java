c5c94e10a6d8d7756603181b46f4a5ed31484d92
LUCAS  PEDREIRA BARRETO <17.01106-0@maua.br>
2022-03-31 12:31:06 -0300

    Criação ddos atributos das classes

--- SimuladoAtividade1/src/Conta.java
+++ SimuladoAtividade1/src/Conta.java
@@ -2,5 +2,7 @@
 //Lucas Pedreira Barreto; 17.01106-0
 
 public class Conta {
-    
+    private String idConta;
+    private float saldo;
+
 }
--- SimuladoAtividade1/src/Usuarios.java
+++ SimuladoAtividade1/src/Usuarios.java
@@ -1,3 +1,8 @@
 public class Usuarios {
+    private String nome;
+    private String email;
+    private int senha; //considerando que seja senha do banco com apenas números, 
+                       //não do aplicativo que pode conter letras, números e caracteres especiais.
     
+
 }
